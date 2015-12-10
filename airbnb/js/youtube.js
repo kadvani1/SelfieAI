8f6837fba982216475c3792b06ad6b45a8cbc4ef
Kingsley <kadvani1@gmail.com>
2015-12-10 09:49:45 -0800

    tv + airbnb
    KA

---
+++ airbnb/js/youtube.js
@@ -0,0 +1,29 @@
+function changePhoto(fname) {
+    $("#frame img").attr("src", fname)
+}
+
+// Actions
+    function updateVideo(people) {
+        if (people.length == 1 && people[0].gender == 'male')  {
+            changePhoto('newyork.png')
+            console.log('male')
+        }
+        else if (people.length == 1 &&people[0].emotion=='happiness') {
+            changePhoto('ibiza.png')
+        }
+         else if (people.length == 1 &&people[0].gender=='female') {
+            changePhoto('paris.png')
+        }
+
+    }
+
+    // Happy
+    // people[0].emotion=='happiness'
+
+    // Gender Male
+    // people[0].gender=='male'
+
+    // Smile
+    // people[0].emotion=='happiness'
+
+    
