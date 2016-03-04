b51d4fdb90dea39c5a2d5a2fcf2ba7c7c9681747
Kingsley <kadvani1@gmail.com>
2016-03-04 02:25:10 +0000

    v9
    - Typewriter added

---
+++ v9/js/typed.js
@@ -0,0 +1,18 @@
+var str = "<p>Male <span style='color:red;'>Detected</span> with SelfieAI</p>",
+    i = 0,
+    isTag,
+    text;
+
+(function type() {
+    text = str.slice(0, ++i);
+    if (text === str) return;
+
+    document.getElementById('typewriter').innerHTML = text;
+
+    var char = text.slice(-1);
+    if( char === '<' ) isTag = true;
+    if( char === '>' ) isTag = false;
+
+    if (isTag) return type();
+    setTimeout(type, 80);
+}());
